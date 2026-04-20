aa76e08df36cd1ee326e32132bae2459a1bcd6c1
mohamed mouatakid <mouatakidmohamed@gmail.com>
2026-04-20 07:35:44 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,21 @@
 /*
 sakila is a database used to collect information about ____ and ____ for use by ____ .
 */
+
+USE sakila
+
+SELECT * FROM actor; -- Retrieved [number] records 
+
+SELECT * FROM film;
+
+SELECT * FROM film; -- Retrieved [number] records 
+
+
+
+
+
+
+
+
+
+
