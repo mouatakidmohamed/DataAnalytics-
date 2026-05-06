7db6f13102a0a357f929aaeab788f97f85662faf
mohamed mouatakid <mouatakidmohamed.com>
2026-05-06 06:01:22 -0400

    mm

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,8 +1,1 @@
 print('Hello world!')
-klkl  
-
-git config --global user.name "mohamed mouatakid"
-git config --global user.email "mouatakidmohamed.com"
-
-git config --global user.name
-git config --global user.email
